95637bf1c83d8f913c4bb120df7f7ba3897b3579
jyuill <john@fig4.com>
2017-01-22 15:07:03 -0800

    add histogram to slider input example

---
+++ 1-sliderInput-histogram/app.R
@@ -0,0 +1,20 @@
+
+library(shiny)
+
+# Create slider input
+?sliderInput
+ui <- fluidPage(
+  sliderInput(inputId="slider",
+              label="Pick a Numbah! (to determine how many random observations)",
+              min=1,max=20,value=10),
+  plotOutput("hist")
+  )
+
+server <- function(input,output) {
+  output$hist <- renderPlot({
+    hist(rnorm(input$slider,mean=50,sd=5))
+  })
+}
+
+shinyApp(ui=ui, server=server)
+
