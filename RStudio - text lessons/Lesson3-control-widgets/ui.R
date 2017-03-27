951a4f6e858805242959cf5ff621684123530fa2
jyuill <john@fig4.com>
2017-03-26 21:26:46 -0700

    control widgets

--- RStudio - text lessons/Lesson3-control-widgets/ui.R
+++ RStudio - text lessons/Lesson3-control-widgets/ui.R
@@ -1,4 +1,4 @@
-#
+# RStudio Text Tutorial: Lesson 3
 # This is the user-interface definition of a Shiny web application. You can
 # run the application by clicking 'Run App' above.
 #
@@ -9,25 +9,97 @@
 
 library(shiny)
 
-# Define UI for application that draws a histogram
+# control widgets
 shinyUI(fluidPage(
+  titlePanel("Basic widgets"),
+  a("RStudio Tutorial (text) - Lesson 3", href="https://shiny.rstudio.com/tutorial/lesson3/"),
   
-  # Application title
-  titlePanel("Old Faithful Geyser Data"),
+  fluidRow(
+    
+    column(3,
+           h3("Buttons"),
+           actionButton("action", label = "Action"),
+           br(),
+           br(), 
+           submitButton("Submit")),
+    
+    column(3,
+           h3("Single checkbox"),
+           checkboxInput("checkbox", label = "Choice A", value = TRUE)),
+    
+    column(3, 
+           checkboxGroupInput("checkGroup", 
+                              label = h3("Checkbox group"), 
+                              choices = list("Choice 1" = 1, 
+                                             "Choice 2" = 2, "Choice 3" = 3),
+                              selected = 1)),
+    
+    column(3, 
+           dateInput("date", 
+                     label = h3("Date input"), 
+                     value = "2014-01-01"))   
+  ),
+  hr(),
   
-  # Sidebar with a slider input for number of bins 
-  sidebarLayout(
-    sidebarPanel(
-       sliderInput("bins",
-                   "Number of bins:",
-                   min = 1,
-                   max = 50,
-                   value = 30)
+  fluidRow(
+    
+    column(3,
+           dateRangeInput("dates", label = h3("Date range"))),
+    
+    column(3,
+           fileInput("file", label = h3("File input"))),
+    
+    column(3, 
+           h3("Help text"),
+           helpText("Note: help text isn't a true widget,", 
+                    "but it provides an easy way to add text to",
+                    "accompany other widgets.")),
+    
+    column(3, 
+           numericInput("num", 
+                        label = h3("Numeric input"), 
+                        value = 1))   
+  ),
+  hr(),
+  
+  fluidRow(
+    
+    column(3,
+           radioButtons("radio", label = h3("Radio buttons"),
+                        choices = list("Choice 1" = 1, "Choice 2" = 2,
+                                       "Choice 3" = 3),selected = 1)),
+    
+    column(3,
+           selectInput("select", label = h3("Select box"), 
+                       choices = list("Choice 1" = 1, "Choice 2" = 2,
+                                      "Choice 3" = 3), selected = 1)),
+    
+    column(3, 
+           sliderInput("slider1", label = h3("Sliders"),
+                       min = 0, max = 100, value = 50),
+           sliderInput("slider2", "",
+                       min = 0, max = 100, value = c(25, 75))
     ),
     
-    # Show a plot of the generated distribution
-    mainPanel(
-       plotOutput("distPlot")
-    )
+    column(3, 
+           textInput("text", label = h3("Text input"), 
+                     value = "Enter text..."))   
+  ),
+  hr(),
+  h2("Exercises"),
+  p("The above content (to which I added hr lines) is code from the RStudio tutorial page. 
+    Content below is additional recommended exercise."),
+  p("Create maps with info from demographic survey:"),
+  fluidRow(
+    
+    column(3,
+           selectInput("select", label = h3("Select box"), 
+                       choices = list("Choice 1" = 1, "Choice 2" = 2,
+                                      "Choice 3" = 3), selected = 1),
+    sliderInput("slider2", "",
+                min = 0, max = 100, value = c(25, 75)))
+    
   )
+  
 ))
+
