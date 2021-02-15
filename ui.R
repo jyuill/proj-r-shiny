14839a816b42c28de7d49ed02ae83bad7f495fbf
John Yuill <john@fig4.com>
2021-02-15 14:06:15 -0800

    Add files via upload

---
+++ ui.R
@@ -0,0 +1,54 @@
+#
+# This is the user-interface definition of a Shiny web application. You can
+# run the application by clicking 'Run App' above.
+
+library(shiny)
+library(shinydashboard)
+
+header <- dashboardHeader(
+    dropdownMenu(
+        type='messages',
+        messageItem(
+            from='Lucky',
+            message='View the International Space Station',
+            href='https://spotthestation.nasa.gov/sightings/'
+        ),
+        messageItem(
+            from='John',
+            message='Check the FAQs for more',
+            href='https://spotthestation.nasa.gov/faq.cfm'
+        )
+    ),
+    dropdownMenu(
+        type='notifications',
+        notificationItem(
+            text='ISS Overhead!'
+        )
+    )
+)
+
+sidebar <- dashboardSidebar(
+    sidebarMenu(
+        menuItem("Data",
+                 tabName='data'),
+        menuItem("Dashboard",
+                 tabName='dashboard')
+    ),
+    sliderInput(
+        inputId='Height',
+        label='How high?',
+        min=10,
+        max=100,
+        value=50
+    )
+)
+body <- dashboardBody(
+    tabItems(
+        tabItem(tabName='data',
+                'Input data here'),
+        tabItem(tabName='dashboard',
+                'Cool dashboard here')
+    )
+)
+
+ui <- dashboardPage(header, sidebar, body)
